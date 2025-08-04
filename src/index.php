6b6d3929d1c6f13a1bb5530e35de49d445e6fbe7
ubuntu <emanuelprecopio@gmail.com>
2025-08-04 20:38:47 -0300

    version final

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="assets/stylev5.css">
+    <link rel="stylesheet" href="assets/stylev6.css">
     <script>
         function mostrarAlerta(mensaje) {
             alert(mensaje);
